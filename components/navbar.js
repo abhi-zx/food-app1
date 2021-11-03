b6b9ceb5f2ebaa4d33273c445a00e4c1a82ee322
Abhi <saabhiahek@gmail.com>
2021-11-03 22:57:39 +0530

    commit 1

---
+++ components/navbar.js
@@ -0,0 +1,22 @@
+function navbar(){
+    return `  <div class="navbar">
+    <div class="logo">
+        <div class="logo_icon">
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSKwusZEAaTh67zLBi2vsaeLSk30szlQhJ2Og&usqp=CAU" alt="">
+        </div>
+    </div>
+    <div class="page">
+    <span><a href="">Home</a></span>
+    <span><a href="./ofday.html">Recipe of Day</a></span>  
+    <span><a href="./latest.html">Latest</a></span>  
+    </div>
+    <div class="input1">
+        <input type="text"placeholder='Search'>
+        <div class="search_icon">
+            <img src="https://cdn1.iconfinder.com/data/icons/hawcons/32/698956-icon-111-search-512.png" alt="">
+        </div>
+    </div>
+</div>`
+
+}
+export default navbar
